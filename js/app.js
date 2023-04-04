79fb4493222d49650eca718d866537dbe94b231a
juantoranzos <toranzos628@gmail.com>
2023-04-04 15:25:48 -0300

    creo el objeto persona

---
+++ js/app.js
@@ -0,0 +1,10 @@
+class Person {
+    constructor(nombre, edad, DNI, sexo, peso, altura, anio_nacimiento) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.DNI = DNI;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.anio_nacimiento = anio_nacimiento;
+    }
